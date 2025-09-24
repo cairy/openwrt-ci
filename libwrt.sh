88661d12c184257c4cdbd9f65ff746410d52c92d
cairy <cairyli@live.cn>
2025-09-24 09:43:43 +0800

    增加aria2、vsftp和ddns

--- libwrt.sh
+++ libwrt.sh
@@ -1,3 +1,5 @@
-rm -rf package/emortal/luci-app-athena-led
-git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
-chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+#!/bin/bash
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.11.2/g' package/base-files/files/bin/config_generate
+
